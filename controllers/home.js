7ead359906e080cae40ec6849325b31b35f136ed
Atta Rasul <attachaudhury@gmail.com>
2020-09-05 18:39:59 +0500

    changes

--- controllers/home.js
+++ controllers/home.js
@@ -17,6 +17,17 @@ exports.index = async (req, res) => {
    return res.render('home/index',{data:responseobject});
 }
 exports.templates = async (req, res) => {
-   var loggedinuser = await user.findById(req.userid);
-   return res.render('home/templates');
+   var userid = req.cookies['userid'];
+   var responseobject = {};
+   var loggedinuser = await user.findById(userid);
+   if(loggedinuser)
+   {
+      responseobject.isloggedin = true;
+      responseobject.loggedinuser = loggedinuser;
+   }
+   else{
+      responseobject.isloggedin = false;
+      responseobject.loggedinuser = loggedinuser;
+   }
+   return res.render('home/templates',{data:responseobject});
 }
